c757bedf3167c0d73d06bbb3f8be75e0b8833acb
nafiseh <nh.edris@gmail.com>
2024-01-19 02:28:32 +0330

    Fetch data and display on pages
    Fetch data and display on pages(Optimized queries in view)

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+from django.utils import timezone
+
 
 def blog_view(request):
-        return render(request,'blog/blog-home.html') 
+    posts=Post.objects.filter(status=1)
+    context={'posts' : posts} 
+    return render(request,'blog/blog-home.html',context) 
 
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
 
 
+def blog_single(request,pid):
+    post=get_object_or_404(Post,pk=pid,status=1,published_date__lte=timezone.now())   
+    context={'post' : post} 
+    return render(request,'blog/blog-single.html',context)
+
+ 
+
+def test (request):
+     posts=Post.objects.all()  
+     context={'posts' : posts} 
+     return render(request,'test.html',context)    
