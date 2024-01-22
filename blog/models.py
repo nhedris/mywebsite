32f00177070d932a121feae3a8509c18cc3f7cb7
nafiseh <nh.edris@gmail.com>
2024-01-22 11:23:51 +0330

    Fixing the rss feed problem
    Fixing the rss feed problem

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    def get_abdolute_url(self):
+    def get_absolute_url(self):
         return reverse ('blog:single',kwargs={'pid': self.id})
       
 class Comment (models.Model):
