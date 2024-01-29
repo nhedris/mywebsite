03211f8674609ccd48ae60ca9cf64fe3df8f0d52
nafiseh <nh.edris@gmail.com>
2024-01-29 10:33:55 +0330

    fasle10
    fasle 10

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('signup/' , views.signup_view , name='signup'),
     path('password_reset', views.PasswordReset.as_view(), name='password_reset'),
     path('password_reset/done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('password_reset_confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
 
 
